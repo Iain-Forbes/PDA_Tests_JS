b3692d94317fe44cef6a80f047231d0bb3b2eb98
Iain Forbes <i.forbes77@gmail.com>
2021-01-22 20:23:50 +0000

    All tests

---
+++ tests/integration/tests.js
@@ -0,0 +1,85 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const { element } = require('protractor');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('calculator functionality', function() {
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://localhost:3000');
+  });
+
+  // write integration tests here in the form of "it should do something..."
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('should update display with the result of operation', function(){
+    running_total = element(by.css('#running_total'))
+
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
+
+  it('should handle numerous operations', function(){
+    running_total = element(by.css('#running_total'))
+
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css("#number6")).click();
+    element(by.css('#operator_equals')).click();
+
+    expect(running_total.getAttribute('value')).to.eventually.equal('-2')
+  })
+
+  it('should handle large numbers ', function(){
+    running_total = element(by.css('#running_total'))
+
+    element(by.css('#number9')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number1')).click();
+
+    element(by.css('#operator_multiply')).click();
+
+    element(by.css('#number9')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number1')).click();
+
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('81018001')
+  })
+
+  it('should output decimals', function(){
+    running_total = element(by.css('#running_total'))
+
+    element(by.css('#number7')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+
+    expect(running_total.getAttribute('value')).to.eventually.equal('3.5')
+  })
+
+  it('should not divide by 0', function(){
+    running_total = element(by.css('#running_total'))
+    
+    element(by.css('#number9')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    
+    expect(running_total.getAttribute('value')).to.eventually.equal("Cannot Divide by Zero")
+  })
+
+});
---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,80 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert');
+const { strictEqual } = require('assert');
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it can add 1 to 4 and get 5', function(){
+    calculator.previousTotal = 1
+    calculator.add(4)
+    assert.strictEqual(calculator.runningTotal, 5)
+  })
+
+  it('it can subtract 4 from 7 and get 3', function(){
+    calculator.previousTotal = 7
+    calculator.subtract(4)
+    assert.strictEqual(calculator.runningTotal, 3)
+  })
+
+  it('it can multiply 3 by 5 and get 15', function(){
+    calculator.previousTotal = 3
+    calculator.multiply(5)
+    assert.strictEqual(calculator.runningTotal, 15)
+  })
+
+  it('it can divide 21 by 7 and get 3', function(){
+    calculator.previousTotal = 21
+    calculator.divide(7)
+    assert.strictEqual(calculator.runningTotal, 3)
+  })
+
+  it('it can divide 21 by 7 and get 3', function(){
+    calculator.previousTotal = 21
+    calculator.divide(7)
+    assert.strictEqual(calculator.runningTotal, 3)
+  })
+
+  it('it can concatenate multiple number button clicks', function(){
+    calculator.numberClick(1)
+    calculator.numberClick(1)
+    calculator.numberClick(8)
+    assert.strictEqual(calculator.runningTotal, 118)
+  })
+
+  it('it chain multiple operations together', function(){
+    calculator.previousTotal = 0 
+    
+    calculator.numberClick(10)
+    calculator.operatorClick("+")
+    calculator.numberClick(10)
+    calculator.operatorClick("*")
+    calculator.numberClick(10)
+    calculator.operatorClick("=")
+
+    assert.strictEqual(calculator.runningTotal, 200)
+  })
+
+  it('clear the running total without affecting the calculation', function(){
+    
+    calculator.numberClick(2)
+    calculator.operatorClick("+")
+    calculator.numberClick(2)
+    calculator.operatorClick("*")
+    
+    calculator.numberClick(9001)
+    calculator.clearClick()
+    
+    calculator.numberClick(5)
+    calculator.operatorClick("=")
+
+    assert.strictEqual(calculator.runningTotal, 20)
+  })
+
+
+
+
+});
